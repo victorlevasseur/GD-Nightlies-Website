ed87f990c233d3a1312c0559151c90746dae2d54
Victor Levasseur <victorlevasseur52@gmail.com>
2016-11-08 10:59:48 +0100

    Add nightly listing

--- index.php
+++ index.php
@@ -1,11 +1,57 @@
-<html>
-<body>
-<p>Salut !</p>
-
 <?php
 
-phpinfo();
+function endsWith( $str, $sub )
+{
+   return ( substr( $str, strlen( $str ) - strlen( $sub ) ) === $sub );
+}
 
 ?>
-</body>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GDevelop nightlies</title>
+    </head>
+    <body>
+        <h1>GDevelop nightlies</h1>
+        <p>
+            GDevelop nightlies are built from the source repository of GDevelop.
+            A new nightly build is produced each day.
+        </p>
+        <p>
+            <strong>
+                Please note that these versions may be instable or not work at all.
+            </strong>
+        </p>
+        <h2>Windows</h2>
+        <p>
+            Here is the list of the available nightlies of GDevelop. They are sorted
+            from the newest to the oldest one.
+        </p>
+        <ul>
+            <?php
+            $files = scandir( "files", SCANDIR_SORT_DESCENDING );
+            $nightly_installer = [];
+
+            foreach( $files as $file )
+            {
+                $matches = [];
+                if( preg_match( $file, "^gdevelop-([0-9]{2}[0-9]{2}[0-9]{2})\\.exe$", $matches ) == 1 )
+                {
+                    ?>
+                    <li>
+                        GDevelop built on <?php echo $matches[3] + "-" + $matches[2] + "-" + $matches[1]; ?>
+                    </li>
+                    <?php
+                }
+            }
+            ?>
+        </ul>
+        <h2>Linux</h2>
+        <p>
+            <em>
+                Not available yet!
+            </em>
+        </p>
+    </body>
 </html>
